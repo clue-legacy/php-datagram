6f3b0fce88cb1d76b1892beb2447d605137e8467
Christian Lück <christian@lueck.tv>
2013-03-28 11:33:56 +0100

    Add UNIX/UDG skeleton

---
+++ Datagram/FactoryUnix.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Datagram;
+
+use React\Promise\When;
+
+class FactoryUnix
+{
+    protected $loop;
+
+    public function __construct(LoopInterface $loop)
+    {
+        $this->loop = $loop;
+    }
+
+    public function createClient($path)
+    {
+        $address = $this->createAddress($path);
+
+        $socket = stream_socket_client($address, $errno, $errstr);
+        if (!$socket) {
+            return When::reject(new Exception('Unable to create client socket: ' . $errstr, $errno));
+        }
+
+        return When::resolve(new Client($this->loop, $socket, $address));
+    }
+
+    public function createServer($path)
+    {
+        $address = $this->createAddress($path);
+
+        $socket = stream_socket_server($address, $errno, $errstr, STREAM_SERVER_BIND);
+        if (!$socket) {
+            return When::reject(new Exception('Unable to create server socket: ' . $errstr, $errno));
+        }
+
+        return When::resolve(new Server($this->loop, $socket, $address));
+    }
+
+    private function createAdress($path)
+    {
+        return 'udg://' . $path;
+    }
+}
