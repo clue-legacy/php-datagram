778a78e16a38bbf2fd1663f9891382a7945f01b2
Christian Lück <christian@lueck.tv>
2013-03-29 09:50:46 +0100

    Add option to remove existing socket path

--- Datagram/FactoryUnix.php
+++ Datagram/FactoryUnix.php
@@ -4,9 +4,13 @@ namespace Datagram;
 
 use React\Promise\When;
 use \Exception;
+use \InvalidArgumentException;
 
 class FactoryUnix
 {
+    const DUPLICATE_FAIL = 1;
+    const DUPLICATE_REBIND = 2;
+
     protected $loop;
 
     public function __construct(LoopInterface $loop)
@@ -30,8 +34,21 @@ class FactoryUnix
         return When::resolve(new Client($this->loop, $socket, $address));
     }
 
-    public function createServer($path, $chmod = null)
+    public function createServer($path, $chmod = null, $duplicateMode = self::DUPLICATE_FAIL)
     {
+        /** @url http://stackoverflow.com/questions/7405932/how-to-know-whether-any-process-is-bound-to-a-unix-domain-socket */
+        if (file_exists($path)) {
+            if ($duplicateMode === self::DUPLICATE_FAIL) {
+                return When::reject(new Exception('Unable to create server socket: socket path already exists'));
+            } else if($duplicateMode === self::DUPLICATE_REBIND) {
+                if (unlink($path) === false) {
+                    return When::reject(new Exception('Unable to remove existing socket path'));
+                }
+            } else {
+                return When::reject(new InvalidArgumentException('Invalid duplicate mode given'));
+            }
+        }
+
         $address = $this->createAddress($path);
 
         $socket = stream_socket_server($address, $errno, $errstr, STREAM_SERVER_BIND);
